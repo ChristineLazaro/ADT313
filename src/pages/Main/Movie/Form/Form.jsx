c43546915bbde7298a56e060609923039cb8c8d0
Christine Lazaro <147901105+ChristineLazaro@users.noreply.github.com>
2024-10-28 22:33:22 +0800

    HANDS ON UPDATES (MIDTERM)
    HANDS ON UPDATES (MIDTERM)

---
+++ src/pages/Main/Movie/Form/Form.jsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom';
+
+const Form = () => {
+  let { movieId } = useParams();
+  return (
+    <>
+      <h1>{movieId !== undefined ? 'Edit ' : 'Create '} Movie</h1>
+      <h2>{movieId}</h2>
+      <div> className= 'search-container'</div>
+      <input></input>
+    </>
+  );
+};
+
+export default Form;
