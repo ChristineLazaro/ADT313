2a8238c8d260707b516bf5ed6214a06ae4852d36
adiraaekishea@gmail.com <adiraaekishea@gmail.com>
2024-11-28 09:46:28 +0800

    Initial Commit on new Branch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -32,6 +33,28 @@ function App() {
   return (
     <div className='App'>
       <RouterProvider router={router} />
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> 9db2786 (Initialize project using Create React App)
     </div>
   );
 }
